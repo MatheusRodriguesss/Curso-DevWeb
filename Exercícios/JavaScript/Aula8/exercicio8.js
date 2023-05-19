edd076a1d47c6321428b575de68a2029ee460951
Matheus Rodrigues <contato.matheusrodrigues3008@gmail.com>
2023-05-18 21:14:25 -0300

    adição das pastas e arquivos da aula 8 de javascript

---
+++ Exercícios/JavaScript/Aula8/exercicio8.js
@@ -0,0 +1,12 @@
+// Eventos
+console.clear();
+const superCalculo = () => {
+  alert("Bem vindo aos Super Cálculo! ");
+  console.log("Bem vindo ao Super Cálculo! ");
+};
+const delta = 4 * 4 - 4 * 3 * -5;
+
+const x1 = ((-4 + Math.sqrt(delta)) / (2 * 3)); 
+
+const x2 = ((-4 - Math.sqrt(delta)) / (2* 3));
+// finalizar junto com os demais em casa
