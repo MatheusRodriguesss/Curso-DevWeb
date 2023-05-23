baf8d018a7cf6180b9b8f06089dd3c02a82ba382
Matheus Rodrigues <contato.matheusrodrigues3008@gmail.com>
2023-05-22 21:44:21 -0300

    adição das pastas e arquivos da aula 9

---
+++ Exercícios/JavaScript/Aula9/exercicio1.js
@@ -0,0 +1,3 @@
+let uni = [8, 4, 56 17, 43, 97, 29];
+
+uni.array.forEach(callback(currentValue [,index[, array]]) [, thisArg]);
---
+++ JS/Aula9/main.js
@@ -0,0 +1,22 @@
+function sorteioDaVida() {
+
+    var numerosDoDestino = Math.floor(Math.random() * 25) + 1 ;
+
+    console.log("Número sorteado: " + numerosDoDestino);
+}
+
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
+sorteioDaVida();
